72488595ca662d5b79d1b27aeacd8f1d26f63add
Cristian <cristian_uteq@hotmail.com>
2023-06-13 17:40:45 -0500

    Avance; Procesar datos de la UI

--- app/src/main/java/com/example/holamundois23/MainActivity.java
+++ app/src/main/java/com/example/holamundois23/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.holamundois23;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Switch;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,7 +19,37 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void btEnviar(View view){
+
+
+
+        EditText txtNombre = (EditText)findViewById(R.id.txtNombre);
+        EditText txtPassowrd = (EditText)findViewById(R.id.txtPassword);
+        RadioButton rbMasc = (RadioButton)findViewById(R.id.rbMasc);
+        Switch swNotificacion = (Switch)findViewById(R.id.swNotificaciones);
+
+        String Nombre= txtNombre.getText().toString();
+        String Password= txtPassowrd.getText().toString();
+
+        //Para sacar el género Seleccionado??
+        String Genero =  rbMasc.isChecked()?"Masculino":"Femenino";
+        //Sacar si recibe o no Notificaciones
+        String notificaciones = swNotificacion.isChecked()?"SI":"NO";
+
         Toast.makeText(this.getApplicationContext(),
-                "Click en Botón",Toast.LENGTH_LONG).show();
+                "Nombre: " + Nombre + ", " +
+                        "Password: " + Password + ", " +
+                        "Género: " + Genero + ", " +
+                        "Notificaciones: " + notificaciones,
+                Toast.LENGTH_LONG).show();
+
+        Intent intent = new Intent(MainActivity.this,
+                                    MainActivity2.class);
+        Bundle b = new Bundle();
+        b.putString("NOMBRE", Nombre);
+        b.putString("PASSWORD", Password);
+        b.putString("GENERO", Genero);
+        b.putString("NOTIFICA", notificaciones);
+        intent.putExtras(b);
+        startActivity(intent);
     }
 }
